c524f55db5a8017f9d96884b47eb3c49f395b7b7
stepanpigriteus <avetor-sport@yandex.ru>
2024-10-15 11:23:14 +0500

    add readme, task-list scroll

--- src/components/container/task_container/task_list.container.tsx
+++ src/components/container/task_container/task_list.container.tsx
@@ -1,11 +1,12 @@
 import treeStore from "../../../strores/store";
 import TaskItem from "../../task_item/task_item";
 import { observer } from "mobx-react-lite";
+import "./task_list.css"
 
 const TaskList = observer(() => {
 
     return (
-        <section className={`min-w-[50%] m-0 min-h-[478px] px-4 py-4 shadow-md   ${treeStore.isDarkTheme ? 'bg-[rgba(15,33,63,0.8)] text-black' : 'bg-[rgba(15,33,63,0.95)] text-gray-200'} rounded-l-lg `}>
+        <section className={`min-w-[50%] min-h-[478px] max-h-[478px] overflow-y-auto px-4 py-4 shadow-md overflow-y-auto section-scroll ${treeStore.isDarkTheme ? 'bg-[rgba(15,33,63,0.8)] text-black' : 'bg-[rgba(15,33,63,0.95)] text-gray-200'} rounded-l-lg`}>
             <h3 className="text-2xl text-white font-medium mb-4 text-left">Задачи</h3>
      
             <ul>
